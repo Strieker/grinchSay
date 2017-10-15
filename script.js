0aff5bd66958bcf631e3148fe54a322024a7fb29
SageStrieker <sspink157@gmail.com>
2017-10-14 20:01:09 -0700

    adding first draft of script, styles, and templates

---
+++ script.js
@@ -0,0 +1,15 @@
+
+const textBox = document.getElementById('textBox');
+const repetitionBox = document.getElementById('repetitionBox');
+
+textBox.addEventListener('input', repeatedTextPopulator);
+repetitionBox.addEventListener('input', repeatedTextPopulator);
+
+function repeatedTextPopulator() {
+  const inputText = textBox.value;
+  const repetitionNumber = repetitionBox.value;
+  const repeatedText = (`${inputText} \n`).repeat(repetitionNumber);
+  document.getElementById('result').innerHTML = repeatedText;
+}
+
+
